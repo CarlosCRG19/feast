fb1e6c5105ec6ba802383907865e303feaa62336
CarlosCRG19 <carloscrg19@gmail.com>
2021-07-13 16:26:18 -0500

    feat (basic navbar): created fragments for each screen and implemented a basic BottomNavigationView

---
+++ app/src/main/java/com/example/fbu_app/Fragments/NextVisitsFragment.java
@@ -0,0 +1,32 @@
+package com.example.fbu_app.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fbu_app.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class NextVisitsFragment extends Fragment {
+
+    public NextVisitsFragment() {};
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_next_visits, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/fbu_app/MainActivity.java
+++ app/src/main/java/com/example/fbu_app/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.fbu_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.RequestHeaders;
 import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.fbu_app.Fragments.CreateFragment;
+import com.example.fbu_app.Fragments.ExploreFragment;
+import com.example.fbu_app.Fragments.NextVisitsFragment;
+import com.example.fbu_app.Fragments.ProfileFragment;
 import com.example.fbu_app.helpers.YelpClient;
 import com.example.fbu_app.models.Business;
 import com.example.fbu_app.models.Hour;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
     public YelpClient yelpClient;
 
+    BottomNavigationView bottomNavigationView;
+
     List<Business> businessList;
 
     @Override
@@ -35,62 +49,93 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        yelpClient = new YelpClient();
+        // Object responsible of adding, removing or replacing Fragments in the stack
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
-        Pair<String, String> location = new Pair<>("location", "Seattle");
-        Log.i(TAG, "New query with params: " + location.first + " -> " + location.second);
-        yelpClient.getMatchingBusinesses(new JsonHttpResponseHandler() {
+        // Assign bottom navigation bar from layout
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        // Create listener for bottomNavigationView items
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                JSONObject response = json.jsonObject;
-                try {
-                    JSONArray jsonArray = response.getJSONArray("businesses");
-                    Log.i(TAG, jsonArray.toString());
-                    businessList = Business.fromJsonArray(jsonArray);
-                    Log.i(TAG, "List of Businesses created with size of: " + String.valueOf(businessList.size()));
-
-                    // Test details
-                    Business business = businessList.get(0);
-                    Log.i(TAG, "EXAMPLE BUSINESS");
-                    Log.i(TAG, "YelpID: " + business.getYelpId());
-                    Log.i(TAG, "Name: " + business.getName());
-                    Log.i(TAG, "Address: " + business.getAddress());
-                    Log.i(TAG, "City: " + business.getCity());
-                    Log.i(TAG, "Country: " + business.getCountry());
-                    Log.i(TAG, "Rating: " + String.valueOf(business.getRating()));
-                    business.saveInBackground();
-
-                    String testId = business.getYelpId();
-                    yelpClient.getBusinessDetails(testId, new JsonHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, Headers headers, JSON json) {
-                            try {
-                                JSONObject hours = json.jsonObject.getJSONArray("hours").getJSONObject(0);
-                                List<Hour> hoursList = Hour.fromJsonArray(hours.getJSONArray("open"), business);
-                                hoursList.get(0).saveInBackground();
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-
-                        }
-                    });
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                Fragment fragment;
+                // Check which item was selected using ids
+                switch (item.getItemId()) {
+                    case R.id.action_history: // Launch compose fragment to create a post
+                        fragment = new NextVisitsFragment();
+                        break;
+                    case R.id.action_profile: // Launch profile fragment (for current user)
+                        // Assign fragment with new args
+                        fragment = new ProfileFragment();
+                        break;
+                    default: // By default, go to main feed
+                        fragment = new CreateFragment();
+                        break;
                 }
-
+                // Change to selected fragment using manager
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
+        });
+        // Set home option as default
+        bottomNavigationView.setSelectedItemId(R.id.action_explore);
 
-            @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-
-            }
-        }, location);
+//        yelpClient = new YelpClient();
+//
+//        Pair<String, String> location = new Pair<>("location", "Seattle");
+//        Log.i(TAG, "New query with params: " + location.first + " -> " + location.second);
+//        yelpClient.getMatchingBusinesses(new JsonHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Headers headers, JSON json) {
+//                JSONObject response = json.jsonObject;
+//                try {
+//                    JSONArray jsonArray = response.getJSONArray("businesses");
+//                    Log.i(TAG, jsonArray.toString());
+//                    businessList = Business.fromJsonArray(jsonArray);
+//                    Log.i(TAG, "List of Businesses created with size of: " + String.valueOf(businessList.size()));
+//
+//                    // Test details
+//                    Business business = businessList.get(0);
+//                    Log.i(TAG, "EXAMPLE BUSINESS");
+//                    Log.i(TAG, "YelpID: " + business.getYelpId());
+//                    Log.i(TAG, "Name: " + business.getName());
+//                    Log.i(TAG, "Address: " + business.getAddress());
+//                    Log.i(TAG, "City: " + business.getCity());
+//                    Log.i(TAG, "Country: " + business.getCountry());
+//                    Log.i(TAG, "Rating: " + String.valueOf(business.getRating()));
+//                    business.saveInBackground();
+//
+//                    String testId = business.getYelpId();
+//                    yelpClient.getBusinessDetails(testId, new JsonHttpResponseHandler() {
+//                        @Override
+//                        public void onSuccess(int statusCode, Headers headers, JSON json) {
+//                            try {
+//                                JSONObject hours = json.jsonObject.getJSONArray("hours").getJSONObject(0);
+//                                List<Hour> hoursList = Hour.fromJsonArray(hours.getJSONArray("open"), business);
+//                                hoursList.get(0).saveInBackground();
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//
+//                        @Override
+//                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+//
+//                        }
+//                    });
+//
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+//
+//            }
+//        }, location);
 
     }
 }
